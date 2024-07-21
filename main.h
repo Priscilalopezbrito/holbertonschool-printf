cd88d1ee9b6c84027518f51ec9022d730c439476
Pirataninja777 <9483@holbertonstudents.com>
2024-07-20 22:09:30 -0400

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef P
+#define P
 #include <stdarg.h>
 
-int _printf (const char *format, ...)
+int _printf(const char *format, ...);
+
+int (*get_fs_function(char s))(va_list);
+
+typedef struct pf
+{
+char *format_specifier;
+int (*funcpoint)(va_list);
+} print_fs;
+
+int print_char(va_list);
+
+int print_string(va_list);
+
+int print_i_d(va_list);
+
+int _putchar(char c);
 
 #endif
+
