8f1aca57c3ac1d264902d778273d2ef280a1cfa8
Priscila Lopez <prisscilalb@gmail.com>
2024-07-21 17:47:35 -0400

    simplify helper function

--- helper_functions.c
+++ helper_functions.c
@@ -44,48 +44,30 @@ int print_string(va_list args)/*count char */
 int print_i_d(va_list args)
 {
 	int num = va_arg(args, int);
-	int counter = 0;/*count*/
-	int div = 1;/*divisor*/
-	int temp;/*store num info*/
+	int counter = 0;
+	int div = 1;
+	int temp;
 
-	if (num < 0)/*handle negative numbers*/
+	if (num < 0)
 	{
-		_putchar('-'); /*Print '-' sign*/
+		_putchar('-');
 		counter++;
-		if (num == INT_MIN)/*-2147483647 - 1*/
+		if (num == INT_MIN)
 		{
-			num = -(num + 1); /* Convert to + and add 1 for overflow*/
-			temp = num;/*stores num in temp*/
-			while (temp >= 10)/*check divisor to determine length*/
-			{
-				temp = temp / 10;/*10/10 = 1*/
-				div = div * 10;/* 1 * 10 = 10*/
-			}
-			while (div != 0)/*prints digits 64-76*/
-			{
-				int digit = num / div;
-
-				if (div == 1)/*last num print correct*/
-				{
-					digit++;
-				}
-				_putchar('0' + digit);
-				num = num % div;
-				div = div / 10;
-				counter++;
-			}
-			return (counter);
+			num = -(num + 1);
 		}
 		else
-			num = -num; /*number = -number*/
+		{
+			num = -num;
+		}
 	}
 	temp = num;
-	while (temp >= 10)/**/
+	while (temp >= 10)
 	{
 		temp = temp / 10;
 		div = div * 10;
 	}
-	while (div != 0)/*print digit 1 by 1*/
+	while (div != 0)
 	{
 		int digit = num / div;
 
@@ -94,8 +76,9 @@ int print_i_d(va_list args)
 		div = div / 10;
 		counter++;
 	}
-	return (counter); /* Number of digits printed*/
+	return (counter);
 }
+
 
 /**
  * _putchar - writes the character c to stdout
